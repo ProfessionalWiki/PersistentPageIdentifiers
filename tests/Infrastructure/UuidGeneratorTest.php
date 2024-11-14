17507a339ea814c0c248ae2611250e2849aaaad9
Waseem Abbas <waximabbax@gmail.com>
2024-11-14 17:13:01 +0500

    make test compatible for all php and phpunit versions

--- tests/Infrastructure/UuidGeneratorTest.php
+++ tests/Infrastructure/UuidGeneratorTest.php
@@ -4,6 +4,7 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\PersistentPageIdentifiers\Tests\Infrastructure;
 
+use PHPUnit\Framework\Constraint\RegularExpression;
 use PHPUnit\Framework\TestCase;
 use ProfessionalWiki\PersistentPageIdentifiers\Infrastructure\UuidGenerator;
 
@@ -13,9 +14,9 @@ use ProfessionalWiki\PersistentPageIdentifiers\Infrastructure\UuidGenerator;
 class UuidGeneratorTest extends TestCase {
 
 	public function testGeneratesValidUuid7(): void {
-		$this->assertRegExp(
-			'/^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/',
-			( new UuidGenerator() )->generate()
+		$this->assertThat(
+			( new UuidGenerator() )->generate(),
+			new RegularExpression( '/^[0-9a-f]{8}-[0-9a-f]{4}-7[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/' )
 		);
 	}
 

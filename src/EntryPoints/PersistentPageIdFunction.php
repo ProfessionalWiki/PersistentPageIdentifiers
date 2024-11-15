5712bffc67ce2babe9413a6b891b335fa02ab605
Waseem Abbas <waximabbax@gmail.com>
2024-11-16 00:49:26 +0500

    Show ID immediately

--- src/EntryPoints/PersistentPageIdFunction.php
+++ src/EntryPoints/PersistentPageIdFunction.php
@@ -4,17 +4,19 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\PersistentPageIdentifiers\EntryPoints;
 
+use MediaWiki\MediaWikiServices;
 use MediaWiki\Page\PageReference;
 use Parser;
 use ProfessionalWiki\PersistentPageIdentifiers\Application\PersistentPageIdentifiersRepo;
+use ProfessionalWiki\PersistentPageIdentifiers\PersistentPageIdentifiersExtension;
 use ProfessionalWiki\PersistentPageIdentifiers\Presentation\PersistentPageIdFormatter;
 use Title;
+use WikiPage;
 
 class PersistentPageIdFunction {
 
 	public function __construct(
-		private readonly PersistentPageIdentifiersRepo $repo,
-		private readonly PersistentPageIdFormatter $idFormatter,
+		private readonly PersistentPageIdentifiersRepo $repo, private readonly PersistentPageIdFormatter $idFormatter,
 	) {
 	}
 
@@ -28,8 +30,17 @@ class PersistentPageIdFunction {
 			return [];
 		}
 
+		$title = Title::castFromPageReference( $page );
+		$id = $this->getPersistentIdForPage( $page );
+
+		if ( $id === null && $title->exists() ) {
+			$pageId = $title->getArticleID();
+			$id = PersistentPageIdentifiersExtension::getInstance()->getIdGenerator()->generate();
+			$this->repo->savePersistentIds( [ $pageId => $id ] );
+		}
+
 		return [
-			htmlspecialchars( $this->idFormatter->format( $this->getPersistentIdForPage( $page ) ) ),
+			htmlspecialchars( $this->idFormatter->format( $id ) ),
 			'noparse' => true,
 			'isHTML' => false,
 		];

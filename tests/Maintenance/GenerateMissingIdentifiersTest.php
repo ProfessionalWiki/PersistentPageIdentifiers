a777f3b563f25b7f8372fe0fc29b7ddf78a668f0
Morne Alberts <morne@malberts.net>
2024-11-17 15:26:19 +0200

    Added maintenance script tests

--- tests/Maintenance/GenerateMissingIdentifiersTest.php
+++ tests/Maintenance/GenerateMissingIdentifiersTest.php
@@ -4,24 +4,64 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\PersistentPageIdentifiers\Tests\Maintenance;
 
-use MediaWiki\Tests\Maintenance\MaintenanceBaseTestCase;
+use Maintenance;
 use ProfessionalWiki\PersistentPageIdentifiers\Maintenance\GenerateMissingIdentifiers;
+use ProfessionalWiki\PersistentPageIdentifiers\Tests\PersistentPageIdentifiersIntegrationTest;
 
 /**
  * @covers \ProfessionalWiki\PersistentPageIdentifiers\Maintenance\GenerateMissingIdentifiers
  * @group Database
  */
-class GenerateMissingIdentifiersTest extends MaintenanceBaseTestCase {
+class GenerateMissingIdentifiersTest extends PersistentPageIdentifiersIntegrationTest {
 
-	protected function getMaintenanceClass(): string {
-		return GenerateMissingIdentifiers::class;
+	private Maintenance $maintenance;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->maintenance = new GenerateMissingIdentifiers();
 	}
 
-	// TODO
-	public function testRuns() {
+	protected function tearDown(): void {
+		$this->maintenance->cleanupChanneled();
+		parent::tearDown();
+	}
+
+	public function testGeneratesNothingIfThereAreNoPages() {
 		$this->maintenance->execute();
 
-		$this->expectOutputRegex( '/Generated/' );
+		$this->expectOutputRegex( '/Generated 0 persistent IDs/' );
+	}
+
+	public function testGeneratesNothingIfAllPagesHavePersistentIds() {
+		$this->createPageWithText();
+		$this->createPageWithText();
+
+		$this->maintenance->execute();
+
+		$this->expectOutputRegex( '/Generated 0 persistent IDs/' );
+	}
+
+	public function testGeneratesIdsIfThereArePagesWithoutPersistentIds() {
+		$this->disablePageSaveHook();
+		$this->createPageWithText();
+		$this->createPageWithText();
+
+		$this->maintenance->execute();
+
+		$this->expectOutputRegex( '/Generated 2 persistent IDs/' );
+	}
+
+	public function testGeneratesSomeIdsIfThereAreSomePagesWithoutPersistentIds() {
+		$this->createPageWithText();
+		$this->createPageWithText();
+
+		$this->disablePageSaveHook();
+		$this->createPageWithText();
+
+		$this->maintenance->execute();
+
+		$this->expectOutputRegex( '/Generated 1 persistent IDs/' );
 	}
 
 }
